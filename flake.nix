4a1f289b222c8bfaa436d718216e1f94bcb3b92c
Rishab Arora <262556157+25f2008178@users.noreply.github.com>
2026-02-28 18:30:36 +0530

    better shell hook to automatically create .venv folder

--- flake.nix
+++ flake.nix
@@ -22,7 +22,13 @@
       ];
 
       shellHook = ''
-        source .venv/bin/activate
+        if [ ! -d ".venv" ]; then
+            python3 -m venv .venv
+            source .venv/bin/activate
+            pip3 install -r requirements.txt
+        else
+            source .venv/bin/activate
+        fi
       '';
     };
   };
